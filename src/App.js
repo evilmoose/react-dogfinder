2c448ecf0d2c60b9fd78c0240a543a5e47d0157a
Evilmoose <ronnielopez@gmail.com>
2024-04-27 16:28:23 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
+
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Good luck!</h1>
+    </div>
+  );
+}
+
+export default App;
