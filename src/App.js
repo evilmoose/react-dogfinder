de7c8a79fd873bd3e1096e5658c8fc1d5e039c50
Evilmoose <ronnielopez@gmail.com>
2024-04-27 17:24:16 -0700

    Added new App Component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
 
-
-
+import RouteList from "./RouterList";
+import NavBar from "./NavBar";
 
 import './App.css';
+import "./portal.css";
 
-function App() {
+const App = () => {
+  const [dogs, setDogs] = useState({
+    data: null,
+    isLoading: true
+  });
+
+  useEffect(() => {
+    async function loadDogs(){
+      const response = await axios.get("http://localhost:5001/dogs")
+      setDogs({
+        data: response.data,
+        isLoading: false
+      })
+    }
+    loadDogs()
+  }, [])
+
+  if (dogs.isLoading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
-    <div className="App">
-      <h1>Good luck!</h1>
+    <div>
+      <h1>Welcome!</h1>
+      <BrowserRouter>
+        <NavBar dogs={dogs.data} />
+        <div className="container">
+          <RouteList dogs={dogs.data} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom"
+import "./portal.css"
+
+function NavBar({ dogs }) {
+  const links = dogs.map(dog => (
+    <NavLink key={dog.name} to={`/dogs/${dog.name.toLowerCase()}`}> 
+        {dog.name}
+    </NavLink>
+  ));
+
+  return (
+    <nav>
+      <NavLink to="/dogs" end>
+        Home
+      </NavLink>
+      {links}
+    </nav>
+  );
+}
+
+export default NavBar;
